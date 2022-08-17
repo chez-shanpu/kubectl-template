56febbbe0b600c49e9dcc0d4a0800fc567bb8d6c
chez-shanpu <tomoki.sugiura@mail.shanpu.info>
2022-08-17 11:27:55 +0900

    Add custom resource template

--- pkg/resource/template.go
+++ pkg/resource/template.go
@@ -1,5 +1,15 @@
 package resource
 
+import (
+	"os"
+	"path"
+)
+
+func TemplateDir() string {
+	home, _ := os.UserHomeDir()
+	return path.Join(home, ".kube/templates")
+}
+
 type Template interface {
 	Generate() (string, error)
 }
